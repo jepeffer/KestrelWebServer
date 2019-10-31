d6aa70fa6921cf30143aea81aa37a8f1ce5d8bbf
Julian Peffer <jepeffer@unomaha.edu>
2019-10-30 19:44:29 -0500

    init

---
+++ TestAppp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+namespace KestrelWebServer
+{
+    /// <summary>
+    /// Executing the "dotnet run" command in the application folder will run this app.
+    /// </summary>
+    public class Program
+    {
+        // The default listening address is http://localhost:5000 if none is specified.
+
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+
+
+    }
+}
---
+++ TestAppp/Startup.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+
+namespace KestrelWebServer
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            var serverAddressesFeature =
+                app.ServerFeatures.Get<IServerAddressesFeature>();
+
+            app.UseStaticFiles();
+
+            app.Run(async (context) =>
+            {
+                context.Response.ContentType = "text/html";
+                await context.Response
+                    .WriteAsync("<!DOCTYPE html><html lang=\"en\"><head>" +
+                        "<title></title></head><body><p>WELCOME FRIEND</p>");
+
+                if (serverAddressesFeature != null)
+                {
+                    await context.Response
+                        .WriteAsync("<p>LISTENING HERE: " +
+                            string.Join(", ", serverAddressesFeature.Addresses) +
+                            "</p>");
+                }
+
+                await context.Response.WriteAsync("<p>Request URL: " +
+                    $"{context.Request.GetDisplayUrl()}<p>");
+            });
+        }
+    }
+    
+}
